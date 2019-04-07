031b26b8cb72b617a3423e4eafae72ce1a817f2b
Craig Bishop <craig@craigjb.com>
2019-04-07 13:01:28 -0700

    Add memory.x to build.rs

--- build.rs
+++ build.rs
@@ -3,14 +3,23 @@ use std::fs::File;
 use std::io::Write;
 use std::path::PathBuf;
 fn main() {
-    if env::var_os("CARGO_FEATURE_RT").is_some() {
-        let out = &PathBuf::from(env::var_os("OUT_DIR").unwrap());
-        File::create(out.join("device.x"))
-            .unwrap()
-            .write_all(include_bytes!("device.x"))
-            .unwrap();
-        println!("cargo:rustc-link-search={}", out.display());
-        println!("cargo:rerun-if-changed=device.x");
+    let target = env::var("TARGET").unwrap();
+    if target.starts_with("thumbv6m") || target.starts_with("thumbv6em") {
+        println!("cargo:rustc-cfg=armv6m")
     }
+
+    let out = &PathBuf::from(env::var_os("OUT_DIR").unwrap());
+
+    File::create(out.join("device.x"))
+        .unwrap()
+        .write_all(include_bytes!("device.x"))
+        .unwrap();
+    File::create(out.join("memory.x"))
+        .unwrap()
+        .write_all(include_bytes!("memory.x"))
+        .unwrap();
+    println!("cargo:rustc-link-search={}", out.display());
+    println!("cargo:rerun-if-changed=device.x");
+    println!("cargo:rerun-if-changed=memory.x");
     println!("cargo:rerun-if-changed=build.rs");
 }
